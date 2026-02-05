d03ffb0447452b0234fb9f2c2dd7449da0a6f47c
yang-onraesaiaovno <33168059+yql5213576@users.noreply.github.com>
2026-02-05 23:32:57 +0800

    FPS v0.4.5 for Godot4.6
    0.4.5

--- sci-fi-fps/assets/audios/sword_kill_enemy_audio.gd
+++
@@ -1,5 +0,0 @@
-extends AudioStreamPlayer3D
-
-
-func _on_finished() -> void:
-	queue_free()
--- sci-fi-fps/assets/my_UI/pause_menu/score_board_FFA_global.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-var players_name:Array[String]=[]
-var players_kills:Array[int]=[]
-var players_die:Array[int]=[]
